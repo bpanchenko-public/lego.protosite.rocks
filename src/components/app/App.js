924acfad48f57f73b023f00f035cc359bbbfdbd4
Boris Panchenko <oberon@list.ru>
2020-02-02 21:05:15 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../resources/svg/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
+    </header>
+  );
+}
+
+export default App;
