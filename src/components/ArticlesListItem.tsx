77b501fa07464107484fbd1e5626b1d353b8abad
Boris Panchenko <oberon@list.ru>
2020-03-03 07:30:10 +0300

    render article link and description

--- src/components/ArticlesListItem.tsx
+++ src/components/ArticlesListItem.tsx
@@ -1,21 +1,34 @@
 import React from "react"
 import cn from "classnames"
 
-const ArticlesListItem = ({ data }: Props) => (
-    <section className={cn('o-list__item', { 'is-read': data.isRead})} aria-selected={data.selected ? 'true' : 'false'}>
-        <div className="o-list__cell">{data.title}</div>
-        <div className="o-list__cell"></div>
-    </section>
-)
+const ArticlesListItem = ({ data }: Props) => {
+    const className = cn(
+        'o-article',
+        'o-list__item',
+        {
+            'is-read': data.isRead
+        })
+    
+    return (
+        <section className={className} aria-selected={data.selected ? 'true' : 'false'}>
+            <div className="o-list__cell">
+                <a href={data.link} onClick={event => event.preventDefault()}>{data.title}</a>
+            </div>
+            <div className="o-list__cell">
+                {data.description}
+            </div>
+        </section>
+    )
+}
 
 type Props = {
     data: {
-        title: String,
-        selected?: Boolean,
-        isRead?: Boolean
+        title: string,
+        description: string,
+        link: string,
+        selected?: boolean,
+        isRead?: boolean
     }
 }
-
-/* <a href="" onClick={() => props.onClick(props.item)}></a> */
 
 export default ArticlesListItem
